a55af163215e78c95a1449a1616ebd62a097227f
reneewh-Chan <reneewhchan@gmail.com>
2026-02-18 17:14:41 -0500

    shift register

--- src/nBit_Register.v
+++ src/nBit_Register.v
@@ -1,2 +1,13 @@
-module nBit_register();
+module nBit_register(
+    input reg input8, 
+    input wire clk, 
+    input wire reset, 
+    output wire output8);
+    always_ff(posedge clk)
+    begin
+        if(reset == 0)
+            output8 <= 1'b00000000;
+        else
+            output8 <= input8;
+    end
 endmodule
