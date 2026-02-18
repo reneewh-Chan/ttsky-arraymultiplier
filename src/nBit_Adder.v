b57bdc97e78515e8d76d5dfdaad99529168e11e1
KaseyHuang29 <kaseyhuang2006@gmail.com>
2026-02-18 18:03:10 -0500

    Update and rename fullAdder.v to nBit_Adder.v

--- src/nBit_Adder.v
+++ src/nBit_Adder.v
@@ -1,17 +1,18 @@
-module bit16_Adder(A, B, Cout, S);
-  input [15:0] A, B; 
-  output reg Cout;
-  output reg [15:0] S;
+module bit16_Adder #(parameter N = 16)( // maybe will need a 16 and 17 to add the 3 products at the end
+  input [N-1:0] A, B,
+  output reg Cout,
+  output reg [N-1:0] S
+);
+  
   wire Cin;
   assign Cin = 0;
-
-  reg [16:0] C; // stores each carry made by each operation
+  reg [N:0] C; // stores each carry made by each operation
 
   integer k;
   always @ (*)
   begin
     C[0] = Cin;
-    for (k = 0; k < 16; k = k + 1)
+    for (k = 0; k < N; k = k + 1)
       begin
         S[k] = A[k] ^ B[k] ^ C[k]; // the sum is made from XOR of each input
         if (A[k] == B[k]) // carrying the cin -> cout logic
