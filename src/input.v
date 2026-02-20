e18de3a4a66e2c012dd87f97d917f5ac49a697a6
Qianyue-06 <fang.qianyue@gmail.com>
2026-02-20 14:22:48 -0500

    Update input.v

--- src/input.v
+++ src/input.v
@@ -1,28 +1,49 @@
-module input_buffer (
+module input(
     input wire        clk,      
     input wire        reset,      
     input wire [7:0]  data_in,     
-    input wire        data_valid，
+    input wire        data_valid,
     output reg        done,   
-    output reg [7:0]  stored_data [0:17] 
+    output reg [7:0]  A [0:8], 
+    output reg [7:0]  B [0:8]
 );
 
-  reg [4:0] count; 
+    reg [4:0] count;
 
-  always @(posedge clk or negedge reset) begin
-    if (!reset) begin
-            count <= 0;
-            done <= 0;
-        end else if (data_valid && !done) begin
-            stored_data[count] <= data_in;
-            if (count == 17) begin
-                done <= 1'b1;
-                count <= count;
-            end else begin
-                count <= count + 1;
-            end
+always @(posedge clk or negedge reset)
+begin
+    if (!reset) 
+    begin
+        count <= 0;
+        done <= 0;
+        for (int i = 0; i < 9; i++) 
+        begin
+            A[i] <= 8'd0;
+            B[i] <= 8'd0;
+        end
+    end 
+    else if (data_valid && !done) 
+    begin
+        if (count <= 8)
+        begin
+            A[count] <= data_in;
+        end
+        else
+        begin
+            B[count - 9] <= data_in;
+        end
+        
+        if (count == 17) 
+        begin
+            done <= 1'b1;
+        end 
+        else 
+        begin
+            count <= count + 1;
         end
     end
+end
 
 endmodule
+
 
