29321a053f5bbf0996212a4608849ddb80e068e1
reneewh-Chan <reneewhchan@gmail.com>
2026-02-19 20:08:56 -0500

    reg update

--- src/shiftReg_nbit.v
+++ src/shiftReg_nbit.v
@@ -1,3 +1,14 @@
-module shiftReg_nbit(CLOCK_50, size, );
+module shiftReg_nbit(input16, clk, neg_reset, output16);
+    input clk
+    input neg_reset
+    input [15:0] input8
+    output [15:0] output8
+    always @(posedge clk, negedge neg_reset)
+    begin
+        if(reset == 0)
+            output16 <= 16'b0;
+        else
+            output16 <= input16;
+    end
 endmodule
 
