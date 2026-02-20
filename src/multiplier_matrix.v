a22eae6afd01376668e3ab3f8fdff961a88e2ef4
Qianyue-06 <fang.qianyue@gmail.com>
2026-02-20 15:10:36 -0500

    Update multiplier_matrix.v

--- src/multiplier_matrix.v
+++ src/multiplier_matrix.v
@@ -1,1 +1,11 @@
+module matrix_mult_core_top (
+    input wire clk,                    
+    input wire reset,             
+    input wire enable,                  
+    input wire [7:0] A [0:8],         
+    input wire [7:0] B [0:8],         
+    output reg [17:0] C [0:8], 
+    output reg done               
+);
 
+  
