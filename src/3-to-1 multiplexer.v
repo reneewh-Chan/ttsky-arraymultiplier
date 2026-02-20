60d0b8f3175850016996acc9bab0b830403d2418
Qianyue-06 <fang.qianyue@gmail.com>
2026-02-20 15:13:54 -0500

    Update 3-to-1 multiplexer.v

--- src/3-to-1 multiplexer.v
+++ src/3-to-1 multiplexer.v
@@ -1,1 +1,18 @@
+module mux3_1(
+  input  wire [1:0]    sel,
+  input  wire [7:0]    in0,
+  input  wire [7:0]    in1,
+  input  wire [7:0]    in2,
+  output reg  [7:0]    out
+);
 
+    always @(*) begin
+        case (sel)
+            2'b00:   out = in0;
+            2'b01:   out = in1;
+            2'b10:   out = in2;
+            default: out = 8'b0
+        endcase
+    end
+
+endmodule
