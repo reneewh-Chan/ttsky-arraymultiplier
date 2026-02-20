23126162d02a365faadc8fb0db40d1ed729f458d
Qianyue-06 <fang.qianyue@gmail.com>
2026-02-20 15:38:04 -0500

    Update multiplier_matrix.v

--- src/multiplier_matrix.v
+++ src/multiplier_matrix.v
@@ -1,4 +1,4 @@
-module matrix_mult_core_top (
+module matrix_mult (
     input wire clk,                    
     input wire reset,             
     input wire enable,                  
@@ -7,6 +7,43 @@ module matrix_mult_core_top (
     output reg [17:0] C [0:8], 
     output reg done               
 );
+    
+reg [1:0] k;                                   
+
+wire [15:0] prod [0:8]; 
+
+3-to-1
+
+always @(posedge clk or negedge rst_n) 
+begin
+    if (!reset) 
+    begin
+        k <= 0;
+        done <= 0;
+    end 
+    else if (enable) 
+    begin
+        k <= 0;
+        done <= 0;
+    end 
+    if (k < 2) 
+    begin
+
+    k <= k + 1;
+    end 
+    else if (k == 2) 
+    begin
+
+    
+        done <= 1;  
+    end
+    end
+
+endmodule
+
+
+
+
 
     
   
